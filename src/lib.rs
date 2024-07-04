d45827b2b839aaf46c3d075c191863c21ea28d64
Felix Drodofsky <Drodofsky@protonmail.com>
2024-07-04 13:30:36 +0200

    start impl look_up fn

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,22 @@ pub use headline::Headlines;
 pub use key::{KeyIndex, Keys, PageItemId};
 pub use pages::{Pages, XmlParser};
 pub use pxml::*;
+
+pub fn lock_up(vocab: &str) -> Vec<String> {
+    let mut dict = MonokakidoDict::open().unwrap();
+    let (_, items) = dict.keys.search_exact(vocab).unwrap();
+
+    let mut heads = Vec::new();
+    for id in items {
+        let page = dict.pages.get_page(id).unwrap();
+        let parsed = parse_xml(page);
+        for h in parsed.1 {
+            match h.0 {
+                Head::H(h) => heads.push(h.iter().map(|h| format!("{h} ")).collect()),
+                _ => {}
+            }
+        }
+    }
+
+    heads
+}
