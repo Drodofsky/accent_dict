53ad16e448e9dbe75557194f2227477b535f4db1
Felix Drodofsky <Drodofsky@protonmail.com>
2024-07-05 06:08:04 +0200

    look_up: return Unpacked vocabs

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,8 @@ pub use pages::{Pages, XmlParser};
 pub use pxml::*;
 
 #[pyfunction]
-fn look_up(path: String,vocab: String) -> Py<PyAny> {
-    Python::with_gil(|py| _look_up(&path,&vocab).to_object(py))
+fn look_up(path: String, vocab: String) -> Vec<Unpacked> {
+    _look_up(&path, &vocab)
 }
 
 /// A Python module implemented in Rust.
@@ -30,22 +30,51 @@ fn accent_dict(m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
-fn _look_up(path: &str,vocab: &str) -> Vec<String> {
+fn _look_up(path: &str, vocab: &str) -> Vec<Unpacked> {
     let mut dict = MonokakidoDict::open_with_path(path).unwrap();
     let (_, items) = dict.keys.search_exact(vocab).unwrap();
 
-    let mut heads = Vec::new();
+    let mut unpacked = Vec::new();
     for id in items {
         let page = dict.pages.get_page(id).unwrap();
         let parsed = parse_xml(page);
         println!("{parsed:#?}");
-        for h in parsed.1 {
-            match h.0 {
-                Head::H(h) => heads.push(h.iter().map(|h| format!("{h} ")).collect()),
-                _ => {}
-            }
-        }
+        unpacked.append(&mut unpack_dic_item(parsed))
     }
 
-    heads
+    unpacked
+}
+
+#[pyclass]
+struct Unpacked {
+    #[pyo3(get)]
+    head: String,
+    #[pyo3(get)]
+    kanji: Option<String>,
+}
+
+fn unpack_dic_item(dic_item: DicItem) -> Vec<Unpacked> {
+    let mut unpacked = Vec::new();
+
+    for head_g in dic_item.1 {
+        match head_g.0 {
+            Head::H(h) => {
+                let head = h.iter().map(|h| format!("{h} ")).collect();
+                let mut kanji = None;
+                for i in h {
+                    if let H::HW(s, i) = i {
+                        let mut s = s.chars();
+                        s.next();
+                        if i.is_none() {
+                            s.next_back();
+                            kanji = Some(s.collect())
+                        }
+                    }
+                }
+                unpacked.push(Unpacked { head, kanji })
+            }
+            _ => {}
+        }
+    }
+    unpacked
 }
